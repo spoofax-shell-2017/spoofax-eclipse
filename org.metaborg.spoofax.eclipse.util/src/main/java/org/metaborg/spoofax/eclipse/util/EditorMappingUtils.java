05d1149b6204793bc2ac5befa8b6a378ed186885
Gabriel Konat <gabrielkonat@gmail.com>
2015-12-31 10:14:06 +0100

    Eclipse Mars compatibility

--- org.metaborg.spoofax.eclipse.util/src/main/java/org/metaborg/spoofax/eclipse/util/EditorMappingUtils.java
+++ org.metaborg.spoofax.eclipse.util/src/main/java/org/metaborg/spoofax/eclipse/util/EditorMappingUtils.java
@@ -1,8 +1,11 @@
 package org.metaborg.spoofax.eclipse.util;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 
 import org.apache.commons.lang3.ArrayUtils;
+import org.eclipse.ui.IEditorDescriptor;
 import org.eclipse.ui.IEditorRegistry;
 import org.eclipse.ui.IFileEditorMapping;
 import org.eclipse.ui.internal.registry.EditorDescriptor;
@@ -58,7 +61,45 @@ public class EditorMappingUtils {
     public static FileEditorMapping mapping(String extension, EditorDescriptor editorDescription) {
         final FileEditorMapping mapping = new FileEditorMapping(extension);
         mapping.addEditor(editorDescription);
-        mapping.setDefaultEditor(editorDescription);
+        setDefaultEditor(mapping, editorDescription);
         return mapping;
+    }
+
+    /**
+     * Sets the default editor for given mapping. Compatible with Eclipse Mars and previous versions via reflection.
+     * 
+     * @param mapping
+     *            Mapping to set default editor for.
+     * @param editorDescription
+     *            Default editor to set.
+     */
+    public static void setDefaultEditor(FileEditorMapping mapping, EditorDescriptor editorDescription) {
+        final Method method = getSetDefaultEditorMethod();
+        try {
+            method.invoke(mapping, editorDescription);
+        } catch(IllegalAccessException e) {
+            throw new RuntimeException("Could not set default editor", e);
+        } catch(IllegalArgumentException e) {
+            throw new RuntimeException("Could not set default editor", e);
+        } catch(InvocationTargetException e) {
+            throw new RuntimeException("Could not set default editor", e);
+        }
+    }
+
+    /**
+     * Gets the correct {@link FileEditorMapping#setDefaultEditor} method via reflection. Parameter changed from
+     * EditorDescriptor to IEditorDescriptor in Eclipse Mars.
+     */
+    private static Method getSetDefaultEditorMethod() {
+        final String methodName = "setDefaultEditor";
+        try {
+            return FileEditorMapping.class.getDeclaredMethod(methodName, EditorDescriptor.class);
+        } catch(NoSuchMethodException e1) {
+            try {
+                return FileEditorMapping.class.getDeclaredMethod(methodName, IEditorDescriptor.class);
+            } catch(NoSuchMethodException e2) {
+                throw new RuntimeException("Cannot find setDefaultEditor method via reflection", e2);
+            }
+        }
     }
 }
