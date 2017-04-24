bccc5b6fdb31baa6e3fcd2c998ba88c6f0fca642
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2017-04-24 18:03:46 +0200

    Fix regression in eclipse project inheritance.

--- org.metaborg.spoofax.eclipse.meta/src/main/java/org/metaborg/spoofax/eclipse/meta/project/EclipseLanguageSpec.java
+++ org.metaborg.spoofax.eclipse.meta/src/main/java/org/metaborg/spoofax/eclipse/meta/project/EclipseLanguageSpec.java
@@ -5,18 +5,14 @@ import org.metaborg.spoofax.eclipse.project.EclipseProject;
 import org.metaborg.spoofax.meta.core.config.ISpoofaxLanguageSpecConfig;
 import org.metaborg.spoofax.meta.core.project.ISpoofaxLanguageSpec;
 
-public class EclipseLanguageSpec implements ISpoofaxLanguageSpec {
-    private final EclipseProject project;
+public class EclipseLanguageSpec extends EclipseProject implements ISpoofaxLanguageSpec {
     private final ISpoofaxLanguageSpecConfig config;
 
 
-    public EclipseLanguageSpec(EclipseProject project, ISpoofaxLanguageSpecConfig config) {
-        this.project = project;
+    public EclipseLanguageSpec(FileObject location, ISpoofaxLanguageSpecConfig config,
+            org.eclipse.core.resources.IProject eclipseProject) {
+        super(location, config, eclipseProject);
         this.config = config;
-    }
-
-    @Override public FileObject location() {
-        return project.location();
     }
 
     @Override public ISpoofaxLanguageSpecConfig config() {
